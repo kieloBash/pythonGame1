cbb77865b2d33208e4eff28a30c730020a83932c
Kielo Bash Mercado <kielo_bash_mercado@dlsu.edu.ph>
2023-05-01 15:45:30 +0800

    added Food Class

--- SnakeGame.py
+++ SnakeGame.py
@@ -14,7 +14,15 @@ class Snake:
     pass
 
 class Food:
-    pass
+    
+    def __init__(self):
+    
+        x = random.randint(0,(GAME_WIDTH/SPACE_SIZE)-1) * SPACE_SIZE
+        y = random.randint(0,(GAME_HEIGHT/SPACE_SIZE)-1) * SPACE_SIZE
+        
+        self.coordinates = [x,y]
+        canvas.create_oval(x, y, x+SPACE_SIZE, y+SPACE_SIZE, fill=FOOD_COLOR, tag="food")
+    
 
 def next_turn():
     pass
@@ -45,5 +53,16 @@ screen_width = window.winfo_screenwidth()
 x = int((screen_width/2) - (window_width/2))
 y = int((screen_height/2) - (window_height/2))
 window.geometry(f"{window_width}x{window_height}+{x}+{y}")
+
+
+snake = Snake()
+food = Food()
+
+
+
+
+
+
+
 
 window.mainloop()
