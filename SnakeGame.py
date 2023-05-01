5db8ee7f3bb31e5aac390de350c3337a4fab7fe1
Kielo Bash Mercado <kielo_bash_mercado@dlsu.edu.ph>
2023-05-01 15:38:43 +0800

    setup for games

---
+++ SnakeGame.py
@@ -0,0 +1,49 @@
+from tkinter import *
+import random
+
+GAME_WIDTH = 700
+GAME_HEIGHT = 700
+SPEED = 50
+SPACE_SIZE = 50
+BODY_PARTS = 3
+SNAKE_COLOR = '#00FF00'
+FOOD_COLOR = '#FF0000'
+BACKGROUND_COLOR = '#000000'
+
+class Snake:
+    pass
+
+class Food:
+    pass
+
+def next_turn():
+    pass
+
+def game_over():
+    pass
+
+window = Tk()
+window.title('Snake Game')
+window.resizable(False,False)
+
+score = 0
+direction = 'down'
+
+label = Label(window, text="Score:{}".format(score),font=('consolas',40))
+label.pack()
+
+canvas = Canvas(window,bg=BACKGROUND_COLOR,height=GAME_HEIGHT,width=GAME_WIDTH)
+canvas.pack()
+
+window.update()
+
+window_height = window.winfo_height()
+window_width = window.winfo_width()
+screen_height = window.winfo_screenheight()
+screen_width = window.winfo_screenwidth()
+
+x = int((screen_width/2) - (window_width/2))
+y = int((screen_height/2) - (window_height/2))
+window.geometry(f"{window_width}x{window_height}+{x}+{y}")
+
+window.mainloop()
